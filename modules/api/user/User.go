363c0da04b11189300f0912831b95029b2c0c492
Yosef Gamble <yegamble@gmail.com>
2021-07-20 19:00:41 +1200

    add form response to create a user

--- modules/api/user/User.go
+++ modules/api/user/User.go
@@ -14,24 +14,24 @@ import (
 
 type User struct {
 	gorm.Model
-	ID           uuid.UUID     `json:"id" gorm:"primary_key"`
-	FirstName    string        `json:"first_name" gorm:"type:text" validate:"required,min=1,max=30"`
-	LastName     string        `json:"last_name" gorm:"type:text" validate:"required,min=1,max=30"`
-	Email        string        `json:"email" gorm:unique",type:text" validate:"required,email,min=6,max=32"`
-	Username     string        `json:"username" gorm:"unique" validate:"required,alphanum,min=1,max=32"`
-	Password     string        `json:"password" gorm:"type:text" validate:"required,min=8,max=120"`
-	DisplayName  string        `json:"username" gorm:"unique" validate:"max=50"`
-	DateOfBirth  time.Time     `json:"date_of_birth" gorm:"type:datetime" validate:"required"`
-	Gender       string        `json:"gender" gorm:"type:text"`
-	CurrentCity  string        `json:"current_city" gorm:"type:text"`
-	HomeTown     string        `json:"hometown" gorm:"type:text"`
-	Bio          string        `json:"bio" gorm:"type:text"`
-	ProfilePhoto string        `json:"profile_photo" gorm:"type:text"`
-	HeaderPhoto  string        `json:"header_photo" gorm:"type:text"`
-	Videos       []video.Video `json:"videos" gorm:"type:array"`
-	Subscribers  []*user.User  `json:"subscribers" gorm:"type:array"`
-	PGPKey       string        `json:"pgp_key" gorm:"type:text"`
-	IsBanned     bool          `json:"pgp_key" gorm:"type:bool"`
+	ID           uuid.UUID     `json:"id" json:"id" form:"id" gorm:"primary_key"`
+	FirstName    string        `json:"first_name" form:"first_name" gorm:"type:text" validate:"required,min=1,max=30"`
+	LastName     string        `json:"last_name" form:"last_name" gorm:"type:text" validate:"required,min=1,max=30"`
+	Email        string        `json:"email" form:"email" gorm:unique",type:text" validate:"required,min=6,max=32"`
+	Username     string        `json:"username" form:"username" gorm:"unique" validate:"required,alphanum,min=1,max=32"`
+	Password     string        `json:"password" form:"password" gorm:"type:text" validate:"required,min=8,max=120"`
+	DisplayName  string        `json:"display_name" form:"display_name" gorm:"unique" validate:"max=50"`
+	DateOfBirth  time.Time     `json:"date_of_birth" form:"date_of_birth" gorm:"type:datetime" validate:"required,datetime"`
+	Gender       string        `json:"gender" form:"gender" gorm:"type:text"`
+	CurrentCity  string        `json:"current_city" form:"current_city" gorm:"type:text"`
+	HomeTown     string        `json:"hometown" form:"hometown" gorm:"type:text"`
+	Bio          string        `json:"bio" form:"bio" gorm:"type:varchar"`
+	ProfilePhoto string        `json:"profile_photo" form:"profile_photo" gorm:"type:text"`
+	HeaderPhoto  string        `json:"header_photo" form:"header_photo" gorm:"type:text"`
+	Videos       []video.Video `json:"videos" form:"videos" gorm:"type:array"`
+	Subscribers  []*user.User  `json:"subscribers" form:"subscribers" gorm:"type:array"`
+	PGPKey       string        `json:"pgp_key" form:"pgp_key" gorm:"type:text"`
+	IsBanned     bool          `json:"is_Banned" form:"is_banned" gorm:"type:bool"`
 	LastActive   string        `json:"last_active" gorm:"type:text"`
 	CreatedAt    time.Time
 	UpdatedAt    time.Time
@@ -56,14 +56,14 @@ func RegisterUserFormParser(c *fiber.Ctx) ([]*handler.ErrorResponse, error) {
 
 	var body User
 
-	formErr := ValidateStruct(&body)
-	if formErr != nil {
-		return formErr, nil
-	}
-
 	err := c.BodyParser(&body)
 	if err != nil {
 		return nil, err
+	}
+
+	formErr := ValidateStruct(&body)
+	if formErr != nil {
+		return formErr, nil
 	}
 
 	return nil, nil
